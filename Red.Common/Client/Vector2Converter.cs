8dfa4cb56ecccf1124a56fc2f7c73a03df20f8de
Brandon <122373753+RedMeansWar@users.noreply.github.com>
2024-02-13 12:44:31 -0600

    added Vector2 Convert

---
+++ Red.Common/Client/Vector2Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using CitizenFX.Core;
+
+namespace Red.Common.Client
+{
+    public class Vector2Converter : JsonConverter
+    {
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(Vector2);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            JObject jObject = JObject.Load(reader);
+            float x = (float)jObject["X"];
+            float y = (float)jObject["Y"];
+            return new Vector2(x, y);
+        }
+
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
